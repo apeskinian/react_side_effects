8e5d48269457eb7cd3798e81961e6f8a9671d8cf
Matthew Aitken <matt@itken.net>
2025-06-24 12:24:14 +0100

    Use localStorage to save picked places

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,40 @@
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 
 import Places from './components/Places.jsx';
 import { AVAILABLE_PLACES } from './data.js';
 import Modal from './components/Modal.jsx';
 import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
+import { sortPlacesByDistance } from './loc.js'
+
+// retrieve stored data and map to places for initial state for pickedPlaces
+// located outside the app component to run only once
+const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+const storedPlaces = storedIds.map(id => AVAILABLE_PLACES.find((place) => place.id === id));
 
 function App() {
   const modal = useRef();
   const selectedPlace = useRef();
-  const [pickedPlaces, setPickedPlaces] = useState([]);
+  const [ availablePlaces, setAvailablePlaces ] = useState([]);
+  const [ pickedPlaces, setPickedPlaces ] = useState(storedPlaces);
+
+  // useEffect is only run once the App component has finished executing
+  // the second argument array is used to stop an infinite loop, if the dependency
+  // has changed the effect will run again. A blank one cannot change so it's only run
+  // once.
+  useEffect(() => {
+    // this attempts to get users location
+    // a side effect as not directly related to rendering the output
+    navigator.geolocation.getCurrentPosition((position) => {
+      const sortedPlaces = sortPlacesByDistance(
+        AVAILABLE_PLACES,
+        position.coords.latitude,
+        position.coords.longitude
+      );
+    // set state once location is found which triggers rerendering
+    setAvailablePlaces(sortedPlaces);
+    });
+  }, []);
 
   function handleStartRemovePlace(id) {
     modal.current.open();
@@ -28,13 +53,33 @@ function App() {
       const place = AVAILABLE_PLACES.find((place) => place.id === id);
       return [place, ...prevPickedPlaces];
     });
-  }
+
+    // SIDE EFFECT - not related to rendered code
+    // Storing selected places in browser localstorage
+    // JSON.parse makes a string into a thing, getting previously stored items
+    // with a blank array if none found
+    const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+    // JSON.stringify makes things a string
+    // updating new selections, checks to see if place is already stored first
+    if (storedIds.indexOf(id) === -1) {
+      localStorage.setItem(
+        'selectedPlaces',
+        JSON.stringify([id, ...storedIds])
+        );
+      }
+    }
 
   function handleRemovePlace() {
     setPickedPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
     modal.current.close();
+
+    const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+    localStorage.setItem(
+      'selectedPlaces',
+      JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current))
+    );
   }
 
   return (
@@ -63,7 +108,8 @@ function App() {
         />
         <Places
           title="Available Places"
-          places={AVAILABLE_PLACES}
+          places={availablePlaces}
+          fallbackText='Sorting places by distance...'
           onSelectPlace={handleSelectPlace}
         />
       </main>
